a0ae8f607d0d5549ccb21323a5be85a53efcc8fb
Marco <marcogara24@gmail.com>
2025-07-08 09:11:01 +0200

    first commit

---
+++ src/main/java/com/example/authcore/AuthcoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.authcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthcoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authcore/AuthcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
